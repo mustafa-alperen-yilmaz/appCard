96c3e58b8688a3f985f09611e53bfd8323df7f5f
mustafa-alperen-yilmaz <33522591+mustafa-alperen-yilmaz@users.noreply.github.com>
2023-01-09 00:31:50 +0300

    first commit

---
+++ lib/Fetch/fetch.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/foundation.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:answer_on_card/Datas/datas.dart';
+
+List<Datas> datasParse(String bodyResponse) {
+  var list = json.decode(bodyResponse) as List<dynamic>;
+  List<Datas> datas = list.map((dts) => Datas.fromJson(dts)).toList();
+  return datas;
+}
+
+// fetching the data
+Future<List<Datas>> datasFetch() async {
+  final fetchResponse =
+      await http.get(Uri.parse("https://jsonplaceholder.typicode.com/photos"));
+  if (fetchResponse.statusCode == 200) {
+    return compute(datasParse, fetchResponse.body);
+  } else {
+    throw Exception();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //declare the isBack bool
+  bool isBack = true;
+  double angle = 0;
+
+  void _flip() {
+    setState(() {
+      angle = (angle + pi) % (2 * pi);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF292a3e),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: _flip,
+                child: TweenAnimationBuilder(
+                    tween: Tween<double>(begin: 0, end: angle),
+                    duration: Duration(seconds: 1),
+                    builder: (BuildContext context, double val, __) {
+                      //here we will change the isBack val so we can change the content of the card
+                      if (val >= (pi / 2)) {
+                        isBack = false;
+                      } else {
+                        isBack = true;
+                      }
+                      return (Transform(
+                        //let's make the card flip by it's center
+                        alignment: Alignment.center,
+                        transform: Matrix4.identity()
+                          ..setEntry(3, 2, 0.001)
+                          ..rotateY(val),
+                        child: Container(
+                            width: 309,
+                            height: 474,
+                            child: isBack
+                                ? Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      image: DecorationImage(
+                                        image: AssetImage("assets/back.png"),
+                                      ),
+                                    ),
+                                  ) //if it's back we will display here
+                                : Transform(
+                                    alignment: Alignment.center,
+                                    transform: Matrix4.identity()
+                                      ..rotateY(
+                                          pi), // it will flip horizontally the container
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                        image: DecorationImage(
+                                          image: AssetImage("assets/face.png"),
+                                        ),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          "amk",
+                                          style: TextStyle(
+                                            fontSize: 30.0,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ) //else we will display it here,
+                            ),
+                      ));
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

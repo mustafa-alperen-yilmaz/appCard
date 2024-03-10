048e9afdc9c0af6162a8d2c5e4816f8e2ebb3933
Mustafa alperen yılmaz <33522591+mustafa-alperen-yilmaz@users.noreply.github.com>
2024-03-10 04:01:28 +0300

    new changes also local json used

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:convert';
-import 'Fetch/fetch.dart';
-
+import 'dart:ui';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -27,15 +26,101 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //declare the isBack bool
   bool isBack = true;
   double angle = 0;
-
-  void _flip() {
-    setState(() {
-      angle = (angle + pi) % (2 * pi);
-    });
+  String randomSentence = '';
+  String selectedLanguage = window.locale.languageCode.toLowerCase();
+  String newLanguage = '';
+  String currentRandomSentence = '';
+  bool languageChanged = false;
+  String currentLanguage = '';
+  List<dynamic> languages = [];
+  
+  @override
+  void initState() {
+    super.initState();
+    currentLanguage = selectedLanguage;
+    loadRandomSentence();
   }
+
+ Future<void> loadRandomSentence() async {
+  final jsonString = await rootBundle.loadString('assets/data.json');
+  final dataMap = jsonDecode(jsonString) as Map<String, dynamic>;
+  final languageData = dataMap['büyük_languages'] as List<dynamic>;
+
+  // Find the language with the same 'id' as the current randomLanguage
+  final currentId = languageData.firstWhere(
+    (element) => element[currentLanguage] == currentRandomSentence,
+    orElse: () => null,
+  )?['id'];
+
+  // Shuffle the list
+  languageData.shuffle();
+
+  // Find a language with the same 'id' as the current randomLanguage after shuffling
+  final randomLanguage = (currentId != null)
+      ? languageData.firstWhere(
+          (element) => element['id'] == currentId,
+          orElse: () => languageData.first,
+        )
+      : languageData.first;
+
+  setState(() {
+    randomSentence = randomLanguage[currentLanguage];
+    currentRandomSentence = randomSentence;
+  });
+}
+
+
+  void _flip() async {
+  setState(() {
+    angle = (angle + pi) % (2 * pi);
+    if (isBack == true && languageChanged){
+      loadRandomSentence();
+    }
+  });
+}
+
+  void _showLanguageMenu() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                title: Text('Turkish'),
+                onTap: () {
+                  _changeLanguage('tr');
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('English'),
+                onTap: () {
+                  _changeLanguage('en');
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+  void _changeLanguage(String language) {
+  setState(() {
+    if (selectedLanguage != language) {
+      // Check if the language is different
+      selectedLanguage = language;
+      languageChanged = true;
+      currentLanguage = language;
+    }
+  });
+
+  loadRandomSentence();
+}
 
   @override
   Widget build(BuildContext context) {
@@ -49,88 +134,66 @@ class _HomePageState extends State<HomePage> {
               GestureDetector(
                 onTap: _flip,
                 child: TweenAnimationBuilder(
-                    tween: Tween<double>(begin: 0, end: angle),
-                    duration: Duration(seconds: 1),
-                    builder: (BuildContext context, double val, __) {
-                      //here we will change the isBack val so we can change the content of the card
-                      if (val >= (pi / 2)) {
-                        isBack = false;
-                      } else {
-                        isBack = true;
-                      }
-                      return (Transform(
-                        //let's make the card flip by it's center
-                        alignment: Alignment.center,
-                        transform: Matrix4.identity()
-                          ..setEntry(3, 2, 0.001)
-                          ..rotateY(val),
-                        child: Container(
-                            width: 309,
-                            height: 474,
-                            child: isBack
-                                ? Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      image: DecorationImage(
-                                        image: AssetImage("assets/back.png"),
+                  tween: Tween<double>(begin: 0, end: angle),
+                  duration: Duration(seconds: 1),
+                  builder: (BuildContext context, double val, __) {
+                    if (val >= (pi / 2)) {
+                      isBack = false;
+                    } else {
+                      isBack = true;
+                    }
+                    return Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()
+                        ..setEntry(3, 2, 0.001)
+                        ..rotateY(val),
+                      child: Container(
+                        width: 309,
+                        height: 474,
+                        child: isBack
+                            ? Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  image: DecorationImage(
+                                    image: AssetImage("assets/back.png"),
+                                  ),
+                                ),
+                              )
+                            : Transform(
+                                alignment: Alignment.center,
+                                transform: Matrix4.identity()
+                                  ..rotateY(pi),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    image: DecorationImage(
+                                      image: AssetImage("assets/face.png"),
+                                    ),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      randomSentence,
+                                      style: TextStyle(
+                                        fontSize: 18.0,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
                                       ),
                                     ),
-                                  ) //if it's back we will display here
-                                : Transform(
-                                    alignment: Alignment.center,
-                                    transform: Matrix4.identity()
-                                      ..rotateY(
-                                          pi), // it will flip horizontally the container
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
-                                        image: DecorationImage(
-                                          image: AssetImage("assets/face.png"),
-                                        ),
-                                      ),
-                                      child: Center(
-                                        child: FutureBuilder(
-                                            future: datasFetch(),
-                                            builder: (BuildContext context,
-                                                AsyncSnapshot snapshot) {
-                                              if (snapshot.hasData) {
-                                                return ListView.builder(
-                                                  itemCount: 1,
-                                                  itemBuilder:
-                                                      (BuildContext context,
-                                                          int index) {
-                                                    String name = snapshot
-                                                        .data[index].name;
-                                                    return ListTile(
-                                                        title: Text(name));
-                                                  },
-                                                );
-                                              } else {
-                                                // error message
-                                                return Container(
-                                                  color: Colors.blue,
-                                                  child: Text(
-                                                    "message",
-                                                    style: const TextStyle(
-                                                        color: Colors.white,
-                                                        fontWeight:
-                                                            FontWeight.bold,
-                                                        fontSize: 20),
-                                                  ),
-                                                );
-                                              }
-                                            }),
-                                      ),
-                                    ),
-                                  ) //else we will display it here,
-                            ),
-                      ));
-                    }),
+                                  ),
+                                ),
+                              ),
+                      ),
+                    );
+                  },
+                ),
               )
             ],
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showLanguageMenu,
+        child: Icon(Icons.language),
       ),
     );
   }

d43e7de1bc335ed850c501c44284395e1775eaf4
Mustafa alperen yılmaz <33522591+mustafa-alperen-yilmaz@users.noreply.github.com>
2024-04-03 01:23:24 +0300

    small changes

--- lib/Splash/splash.dart
+++ lib/Splash/splash.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import '../main.dart'; // Ana sayfanızın olduğu dosyaya doğru path'i kullanın
+import '../main.dart'; 
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -9,24 +9,24 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   int _currentIndex = 0;
-  final CarouselController _controller = CarouselController(); // CarouselController eklendi
+  final CarouselController _controller = CarouselController(); 
 
   List<String> splashTexts = [
-    "A",
-    "B",
-    "C", // Carousel içinde gösterilecek metinler
+    "Empty Your Mind",
+    "Focus Your Feelings",
+    "Leave The Rest To Us",
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF292a3e), // Arka plan rengini ayarlar
+      backgroundColor: Color(0xFF292a3e),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           CarouselSlider.builder(
-            carouselController: _controller, // CarouselController buraya atanıyor
+            carouselController: _controller, 
             itemCount: splashTexts.length,
             options: CarouselOptions(
               height: 400.0,
@@ -45,20 +45,19 @@ class _SplashScreenState extends State<SplashScreen> {
                     style: TextStyle(
                       fontSize: 24,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white, // Yazı rengini ayarlar
+                      color: Colors.white, 
                     ),
                   ),
                 ),
               );
             },
           ),
-          SizedBox(height: 20), // Buton ile Carousel arasında boşluk bırakır
+          SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
               if (_currentIndex < splashTexts.length - 1) {
                 _controller.nextPage(duration: Duration(milliseconds: 300), curve: Curves.linear);
               } else {
-                // Carousel'in son sayfasındaysa, ana sayfaya git
                 Navigator.of(context).pushReplacement(
                   MaterialPageRoute(builder: (context) => MyApp()),
                 );
@@ -67,11 +66,11 @@ class _SplashScreenState extends State<SplashScreen> {
             child: Text(
               _currentIndex < splashTexts.length - 1 ? 'Next' : 'Are you ready?',
               style: TextStyle(
-                color: Colors.white, // Buton metni rengi, buton stilinde ayarlanmalıdır.
+                color: Colors.white,
               ),
             ),
             style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFF292a3e), // Butonun arka plan rengi
+              backgroundColor: Color.fromARGB(255, 89, 95, 194),
             ),
           ),
         ],

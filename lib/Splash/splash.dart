eb489ccf90a66fd97ea4cd04fea9d1231ed91ed8
Mustafa alperen yılmaz <33522591+mustafa-alperen-yilmaz@users.noreply.github.com>
2024-04-04 02:10:38 +0300

    splash screen new view

--- lib/Splash/splash.dart
+++ lib/Splash/splash.dart
@@ -1,21 +1,43 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import '../main.dart';
+import '../main.dart'; // MyApp sınıfınızın bulunduğu dosyanın yolu
 
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
   int _currentIndex = 0;
   final CarouselController _controller = CarouselController();
+  late AnimationController _animationController;
+  late Animation<double> _animation;
 
   List<String> splashTexts = [
     "Empty Your Mind",
     "Focus Your Feelings",
     "Leave The Rest To Us",
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+    )..repeat();
+    _animation = Tween<double>(begin: 0.0, end: 2 * math.pi).animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,31 +51,35 @@ class _SplashScreenState extends State<SplashScreen> {
             carouselController: _controller,
             itemCount: splashTexts.length,
             options: CarouselOptions(
-                height: 400.0,
-                autoPlay: false,
-                enlargeCenterPage: true,
-                enableInfiniteScroll: false,
-                scrollPhysics: NeverScrollableScrollPhysics(),
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _currentIndex = index;
-                  });
-                }),
+              height: 400.0,
+              autoPlay: false,
+              enlargeCenterPage: true,
+              enableInfiniteScroll: false,
+              scrollPhysics: NeverScrollableScrollPhysics(),
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              }
+            ),
             itemBuilder: (context, index, realIndex) {
-              return Card(
-                elevation: 8.0,
-                color: Color.fromARGB(255, 81, 74, 190).withOpacity(0.1),
-                child: Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.all(16.0),
-                  child: Center(
-                    child: Text(
-                      splashTexts[index],
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+              return CustomPaint(
+                painter: BorderPainter(_animation.value),
+                child: Card(
+                  elevation: 8.0,
+                  color: Color.fromARGB(255, 81, 74, 190).withOpacity(0.1),
+                  child: Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(
+                      child: Text(
+                        splashTexts[index],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
@@ -76,7 +102,7 @@ class _SplashScreenState extends State<SplashScreen> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 Text(
-                  _currentIndex < splashTexts.length - 1 ? 'Next  ' : 'Are you ready?',
+                  _currentIndex < splashTexts.length - 1 ? 'Next  ' : 'Are You Ready ?',
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 18,
@@ -99,4 +125,33 @@ class _SplashScreenState extends State<SplashScreen> {
       ),
     );
   }
+}
+
+class BorderPainter extends CustomPainter {
+  final double progress;
+  BorderPainter(this.progress);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.blue // Start color
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 5;
+
+    // Use the drawArc method to rotate the gradient around the center
+    paint.shader = SweepGradient(
+      colors: [Colors.white, Color.fromARGB(255, 70, 21, 150), Colors.white], // Color transition
+      startAngle: 0.0,
+      endAngle: math.pi * 2,
+      transform: GradientRotation(progress),
+    ).createShader(Rect.fromLTWH(0, 0, size.width, size.height));
+
+    // Create a circular path that represents the border
+    var path = Path()
+      ..addRect(Rect.fromLTWH(0, 0, size.width, size.height));
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
